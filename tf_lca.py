dc647e4fa74e7f03795342395971cc6c4a752f42
Eric Dodds <edodds828@gmail.com>
2017-03-20 18:04:20 -0700

    .

--- tf_lca.py
+++ tf_lca.py
@@ -124,7 +124,8 @@ class LCALearner(tf_sparsenet.Sparsenet):
         self.renorm_phi = self.phi.assign(tf.nn.l2_normalize(self.phi, dim=1))
 
         self.snr = tf.reduce_mean(tf.square(self.X))/self.mse
-        snr_ratio = self.snr/tf.exp(np.log(10.0)*self.snr_goal/10.0,dtype=tf.float32)
+        snrconvert = tf.constant(np.log(10.0)/10.0, dtype=tf.float32)
+        snr_ratio = self.snr/tf.exp(snrconvert*tf.constant(self.snr_goal,dtype=tf.float32))
         self.seek_snr = self.thresh.assign(self.thresh*tf.pow(snr_ratio,0.1))
         self.snr_db = 10.0*tf.log(self.snr)/np.log(10.0)
         
