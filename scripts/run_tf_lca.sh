50e1ecd2bc9a800e53cc7636f27f21053556615f
Eric Dodds <edodds828@gmail.com>
2017-03-30 15:50:26 -0700

    added tf_lca script for cluster

---
+++ scripts/run_tf_lca.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Partition:
+#SBATCH --partition=cortex
+#
+# Wall clock limit:
+#SBATCH --time=48:00:00
+#
+# Memory:
+#SBATCH --mem-per-cpu=8G
+#
+# Constraint:
+#SBATCH --gres=gpu:1
+cd /global/home/users/edodds/DictLearner
+export MODULEPATH=/global/software/sl-6.x64_64/modfiles/apps:$MODULEPATH
+module load ml/tensorflow/0.11.0rc0
+python scripts/tsn_script.py --data $data --sigma $sigma
+
+python scripts/tf_lca_script.py
